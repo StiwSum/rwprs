c2db9432d44cf984084915872154d1fdbc1fc56b
BadykovRafael <stiwsum@mail.ru>
2019-12-11 08:03:55 +0400

    commit:  Added entry point;  Ama not a monkey;

---
+++ src/RPIS71/Badykov/wdad/Application.java
@@ -0,0 +1,7 @@
+package RPIS71.Badykov.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I’m <Rafael> <Badykov>, and I’m not a monkey");
+    }
+}
