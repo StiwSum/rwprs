fb0fd23ebaf34445b94a5ff3dfca8f06195572a0
BadykovRafael <stiwsum@mail.ru>
2020-01-13 05:28:01 +0400

    commit: final in ConfigurationFeature;

--- src/RPIS71/Badykov/wdad/data/managers/PreferencesManager.java
+++ src/RPIS71/Badykov/wdad/data/managers/PreferencesManager.java
@@ -1,10 +1,12 @@
 package RPIS71.Badykov.wdad.data.managers;
-
+import RPIS71.Badykov.wdad.utils.PreferencesManagerConstants;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.xml.sax.InputSource;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
@@ -13,88 +15,179 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Objects;
-
+import java.util.Properties;
 public class PreferencesManager {
+
+    private static final String FILE_PATH ="src/RPIS71/Badykov/wdad/resources/configuration/appconfig.xml";
     public static final PreferencesManager INSTANCE = new PreferencesManager();
-    private static final String FILE_PATH = "src/RPIS71/Badykov/wdad/resources/configuration/appconfig.xml";
     private static Document document;
 
     private PreferencesManager() {
         try {
-            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(FILE_PATH));
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setValidating(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(new File(FILE_PATH));
         } catch (SAXException | IOException | ParserConfigurationException e) {
             e.printStackTrace();
         }
     }
 
-    public String getCreateRegistry() {
-        return Objects.requireNonNull(getTag("createregistry")).getTextContent();
+    public void setProperty(String key, String value) {
+        Node propertyNode = getNode(key);
+        if (propertyNode != null) {
+            propertyNode.setTextContent(value);
+            changeDocument();
+        }
     }
 
-    public void setCreateRegistry(String newValue) {
-        Objects.requireNonNull(getTag("createregistry")).setTextContent(newValue);
-        changeDocument(document);
+    public String getProperty(String key) {
+        return Objects.requireNonNull(getNode(key)).getTextContent();
     }
 
-    public String getRegistryAddress() {
-        return Objects.requireNonNull(getTag("registryaddress")).getTextContent();
+    public void setProperties(Properties prop) {
+        prop.forEach((key, value) -> setProperty(key.toString(), value.toString()));
     }
 
-    public void setRegistryAddress(String newValue) {
-        Objects.requireNonNull(getTag("registryaddress")).setTextContent(newValue);
-        changeDocument(document);
+    public Properties getProperties() {
+        Properties properties = new Properties();
+        Field[] fields = PreferencesManagerConstants.class.getFields();
+        for (Field field : fields) {
+            try {
+                String fieldValue = String.valueOf(field.get(String.class));
+                properties.setProperty(fieldValue, getProperty(fieldValue));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return properties;
     }
 
-    public String getRegistryPort() {
-        return Objects.requireNonNull(getTag("registryport")).getTextContent();
+    public void addBindedObject(String name, String className) {
+        Node serverElement = document.getElementsByTagName("server").item(0);
+        Element bindedObjectElement = document.createElement("bindedobject");
+        bindedObjectElement.setAttribute("name", name);
+        bindedObjectElement.setAttribute("class", className);
+        serverElement.appendChild(bindedObjectElement);
+        changeDocument();
     }
 
-    public void setRegistryPort(String newValue) {
-        Objects.requireNonNull(getTag("registryport")).setTextContent(newValue);
-        changeDocument(document);
+    public void removeBindedObject(String name) {
+        NodeList bindedObjects = document.getElementsByTagName("bindedobject");
+        Element bindedObjectElement;
+        for (int i = 0; i < bindedObjects.getLength(); i++) {
+            bindedObjectElement = (Element) bindedObjects.item(i);
+            if (name.equalsIgnoreCase(bindedObjectElement.getAttribute("name"))) {
+                bindedObjectElement.getParentNode().removeChild(bindedObjectElement);
+                break;
+            }
+        }
+        changeDocument();
     }
 
-    public String getPolicyPath() {
-        return Objects.requireNonNull(getTag("policypath")).getTextContent();
+    private Node getNode(String key) {
+        try {
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            String expression = key.replaceAll("\\.", "/");
+            return (Node) xpath.evaluate(expression, document, XPathConstants.NODE);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public void setPolicyPath(String newValue) {
-        Objects.requireNonNull(getTag("policypath")).setTextContent(newValue);
-        changeDocument(document);
-    }
-
-    public String getUseCodeBaseOnly() {
-        return Objects.requireNonNull(getTag("usecodebaseonly")).getTextContent();
-    }
-
-    public void setUseCodeBaseOnly(String newValue) {
-        Objects.requireNonNull(getTag("usecodebaseonly")).setTextContent(newValue);
-        changeDocument(document);
-    }
-
-    public String getClassProvider() {
-        return Objects.requireNonNull(getTag("classprovider")).getTextContent();
-    }
-
-    public void setClassProvider(String newValue) {
-        Objects.requireNonNull(getTag("classprovider")).setTextContent(newValue);
-        changeDocument(document);
-    }
-
-    private Element getTag(String tagName) {
-        return (Element) document.getElementsByTagName(tagName).item(0);
-    }
-
-    private void changeDocument(Document doc) {
+    private void changeDocument() {
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformer.transform(new DOMSource(doc), new StreamResult(FILE_PATH));
+            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "appconfig.dtd");
+            transformer.transform(new DOMSource(document), new StreamResult(FILE_PATH));
         } catch (TransformerException e) {
             e.printStackTrace();
         }
 
     }
+
+    @Deprecated
+    public String getCreateRegistry() {
+        return Objects.requireNonNull(getTag("createregistry")).getTextContent();
+    }
+
+    @Deprecated
+    public void setCreateRegistry(String newValue) {
+        Objects.requireNonNull(getTag("createregistry")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    public String getRegistryAddress() {
+        return Objects.requireNonNull(getTag("registryaddress")).getTextContent();
+    }
+
+    @Deprecated
+    public void setRegistryAddress(String newValue) {
+        Objects.requireNonNull(getTag("registryaddress")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    public String getRegistryPort() {
+        return Objects.requireNonNull(getTag("registryport")).getTextContent();
+    }
+
+    @Deprecated
+    public void setRegistryPort(String newValue) {
+        Objects.requireNonNull(getTag("registryport")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    public String getPolicyPath() {
+        return Objects.requireNonNull(getTag("policypath")).getTextContent();
+    }
+
+    @Deprecated
+    public void setPolicyPath(String newValue) {
+        Objects.requireNonNull(getTag("policypath")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    public String getUseCodeBaseOnly() {
+        return Objects.requireNonNull(getTag("usecodebaseonly")).getTextContent();
+    }
+
+    @Deprecated
+    public void setUseCodeBaseOnly(String newValue) {
+        Objects.requireNonNull(getTag("usecodebaseonly")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    public String getClassProvider() {
+        return Objects.requireNonNull(getTag("classprovider")).getTextContent();
+    }
+
+    @Deprecated
+    public void setClassProvider(String newValue) {
+        Objects.requireNonNull(getTag("classprovider")).setTextContent(newValue);
+        changeDocument();
+    }
+
+    @Deprecated
+    private Element getTag(String tagName) {
+        return (Element) document.getElementsByTagName(tagName).item(0);
+    }
+
 }
--- src/RPIS71/Badykov/wdad/data/managers/Test.java
+++ src/RPIS71/Badykov/wdad/data/managers/Test.java
@@ -3,9 +3,5 @@ package RPIS71.Badykov.wdad.data.managers;
 public class Test {
     public static void main(String[] args) {
         PreferencesManager manager = PreferencesManager.INSTANCE;
-        manager.setCreateRegistry("yes");
-        System.out.println(manager.getCreateRegistry());
-        manager.setCreateRegistry("no");
-        System.out.println(manager.getCreateRegistry());
     }
 }
