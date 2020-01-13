8083be137954899e6418d98f475a04464c23603f
BadykovRafael <stiwsum@mail.ru>
2020-01-13 05:52:00 +0400

    commit: final in PreferencesManagerAPIChange;

---
+++ src/RPIS71/Badykov/wdad/learn/rmi/Client.java
@@ -0,0 +1,7 @@
+package RPIS71.Badykov.wdad.learn.rmi;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RPIS71/Badykov/wdad/utils/PreferencesManagerConstants.java
@@ -0,0 +1,18 @@
+package RPIS71.Badykov.wdad.utils;
+
+public interface PreferencesManagerConstants {
+    String CREATE_REGISTRY = "appconfig.rmi.server.registry.createregistry";
+    String REGISTRY_ADDRESS = "appconfig.rmi.server.registry.registryaddress";
+    String REGISTRY_PORT = "appconfig.rmi.server.registry.registryport";
+    String POLICY_PATH = "appconfig.rmi.client.policypath";
+    String USE_CODEBASE_ONLY = "appconfig.rmi.client.usecodebaseonly";
+    String CLASS_PROVIDER = "appconfig.rmi.classprovider";
+    String BINDING_NAME = "XmlDataManager";
+    String CLASS_NAME="appconfig.datasource.classname";
+    String DRIVER_TYPE="appconfig.datasource.drivertype";
+    String HOST_NAME="appconfig.datasource.hostname";
+    String PORT="appconfig.datasource.port";
+    String DBNAME ="appconfig.datasource.dbname";
+    String USER="appconfig.datasource.user";
+    String PASS="appconfig.datasource.pass";
+}
